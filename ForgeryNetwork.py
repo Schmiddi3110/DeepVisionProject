ccb4e3e965c980b533cc702b7e873995d8ed4019
Fabian Schmidt <fepschmidt@gmail.com>
2024-07-02 00:51:51 +0200

    forgery

--- ForgeryNetwork.py
+++ ForgeryNetwork.py
@@ -1,56 +1,121 @@
 import torch
-from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.nn.functional as F
-import torchvision.models as models
 import matplotlib.pyplot as plt
 
-class SiameseSignatureNetwork(nn.Module):
+class ForgeryNetwork(nn.Module):
     def __init__(self):
-        super(SiameseSignatureNetwork, self).__init__()
-
-        # Using a pre-trained ResNet model
-        self.resnet = models.resnet18(pretrained=True)
-        
-        # Modify the first convolutional layer to accept 1-channel input
-        self.resnet.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-        
-        # Get the number of input features for the last fully connected layer
-        num_ftrs = self.resnet.fc.in_features
-        
-        # Remove the final fully connected layer
-        self.resnet.fc = nn.Identity()
-        
-        # Add custom fully connected layers
-        self.fc1 = nn.Sequential(
-            nn.Linear(num_ftrs, 1024),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(1024),
-            nn.Dropout(0.5),
-            
-            nn.Linear(1024, 256),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(256),
-            nn.Dropout(0.5),
-            
-            nn.Linear(256, 2)
+        super(ForgeryNetwork, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1,64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.Sigmoid(),
         )
-        
-    def forward_once(self, x):
-        # This function will be called for both images
-        # Its output is used to determine the similarity
-        output = self.resnet(x)
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3,stride=1,padding=1),
+            nn.BatchNorm2d(64),
+            nn.Sigmoid(),
+            nn.MaxPool2d(2,2),
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1,padding=1),
+            nn.BatchNorm2d(128),
+            nn.Sigmoid(),
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(128,128, kernel_size=3, stride=1,padding=1),
+            nn.BatchNorm2d(128),
+            nn.Sigmoid(),
+            nn.MaxPool2d(2,2),
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(128,256, kernel_size=3, stride=1,padding=1),
+            nn.BatchNorm2d(256),
+            nn.Sigmoid(),
+        )
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(256,256, kernel_size=3, stride=1,padding=1),
+            nn.BatchNorm2d(256),
+            nn.Sigmoid(),
+        )
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(256,256, kernel_size=3, stride=1,padding=1),
+            nn.BatchNorm2d(256),
+            nn.Sigmoid(),
+            nn.MaxPool2d(2,2),
+        )
+        self.conv8 = nn.Sequential(
+             nn.Conv2d(256,512, kernel_size=3, stride=1,padding=1),
+            nn.BatchNorm2d(512),
+            nn.Sigmoid(),
+        )
+        self.conv9 = nn.Sequential(
+            nn.Conv2d(512,512, kernel_size=3, stride=1,padding=1),
+            nn.BatchNorm2d(512),
+            nn.Sigmoid(),
+        )
+        self.conv10 = nn.Sequential(
+            nn.Conv2d(512,512, kernel_size=3, stride=1,padding=1),
+            nn.BatchNorm2d(512),
+            nn.Sigmoid(),
+            nn.MaxPool2d(2,2),
+        )
+        self.conv11 = nn.Sequential(
+            nn.Conv2d(512,512, kernel_size=3, stride=1,padding=1),
+            nn.BatchNorm2d(512),
+            nn.Sigmoid(),
+        )
+        self.conv12 = nn.Sequential(
+            nn.Conv2d(512,512, kernel_size=3, stride=1,padding=1),
+            nn.BatchNorm2d(512),
+            nn.Sigmoid(),
+        )
+        self.conv13 = nn.Sequential(
+            nn.Conv2d(512,512, kernel_size=3, stride=1,padding=1),
+            nn.BatchNorm2d(512),
+            nn.Sigmoid(),
+            nn.MaxPool2d(2,2)
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((7,7))
+        self.fc1 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(25088, 4096),
+            nn.Sigmoid(), 
+        )
+        self.fc2 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(4096,4096),
+            nn.Sigmoid(),
+        )
+        self.fc3 = nn.Sequential(
+            nn.Linear(4096, 100),
+        )
+    def forward_once(self,x):
+        output = self.conv1(x)
+        output = self.conv2(output)
+        output = self.conv3(output)
+        output = self.conv4(output)
+        output = self.conv5(output)
+        output = self.conv6(output)
+        output = self.conv7(output)
+        output = self.conv8(output)
+        output = self.conv9(output)
+        output = self.conv10(output)
+        output = self.conv11(output)
+        output = self.conv12(output)
+        output = self.conv13(output)
+        output = self.avgpool(output)
+        output = torch.flatten(output, 1)
         output = self.fc1(output)
+        output = self.fc2(output)
+        output = self.fc3(output)
         return output
-
-    def forward(self, input1, input2):
-        # In this function we pass in both images and obtain both vectors
-        # which are returned
+    
+    def forward(self,input1, input2):
         output1 = self.forward_once(input1)
         output2 = self.forward_once(input2)
-
         return output1, output2
-
+    
     def evaluate(self, validation_loader, net, criterion):
         net.eval()
         val_loss = 0.0
@@ -61,6 +126,7 @@ class SiameseSignatureNetwork(nn.Module):
                 loss_contrastive = criterion(output1, output2, label)
                 val_loss += loss_contrastive.item()
         return val_loss / len(validation_loader)
+
 
     def train_network(self, train_loader, val_loader, net, optimizer, criterion, epochs, patience=5):
         counter = []
